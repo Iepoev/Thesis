7d27e45145ec22b119e1c0637e5609d1f711f3e3
Iepoev <jeroen.declerck@ugent.be>
2020-05-30 15:41:54 +0200

    export/import userdata werkt + presets

--- implementatie/main.py
+++ implementatie/main.py
@@ -7,20 +7,33 @@ from baecke import baecke
 def main():
   parser = argparse.ArgumentParser(description='Fitness coach.')
 
-  parser.add_argument('-f','--file', help='import user data and model from a file')
-  parser.add_argument('-i','--init', choices=['full', 'unfit', 'fit'], help='initialize user data, either from scratch or a basic fit/unfit user)')
-
-
+  parser.add_argument('-f','--file', const='userdata.hdf5', nargs='?', help='import user data and model from a file')
+  parser.add_argument('-i','--init', default='fromscratch', nargs='?', choices=['fromscratch', 'inactive', 'active', 'elite'], help='initialize user data, either from scratch or a basic fit/unfit user)')
 
   args = parser.parse_args()
   print(args)
 
-  (work_index,sport_index,leisure_index) = baecke()
-
-  u = User(work_index,sport_index,leisure_index)
-
-  print(u)
-
+  if args.file:
+    u = User.fromfile(fname=args.file)
+    print(u)
+  else:
+    if args.init == 'fromscratch':
+      (work_index,sport_index,leisure_index) = baecke()
+      u = User(work_index,sport_index,leisure_index)
+      print(u)
+      u.export_hdf5()
+    elif args.init == 'inactive':
+      u = User(2.0, 2.0, 1.0)
+      print(u)
+      u.export_hdf5()
+    elif args.init == 'active':
+      u = User(2.0, 4.0, 3.0)
+      print(u)
+      u.export_hdf5()
+    elif args.init == 'elite':
+      u = User(4.5, 5.0, 4.0)
+      print(u)
+      u.export_hdf5()
 
 if __name__ == "__main__":
   main()
--- implementatie/user.py
+++ implementatie/user.py
@@ -11,18 +11,16 @@ class User:
     self.baeke_sport = baeke_sport
     self.baeke_leisure = baeke_leisure
 
+  @classmethod
+  def fromfile(cls, fname='userdata.hdf5'):
+    dset = h5py.File(fname, 'r')['model']
+    return cls(dset.attrs['baeke_work'], dset.attrs['baeke_leisure'], dset.attrs['baeke_sport'])
+
   def __str__(self):
     return 'User(baeke_work={0}, baeke_sport={1}, baeke_leisure={2})'.format(self.baeke_work,self.baeke_sport,self.baeke_leisure)
 
   def export_hdf5(self, fname = 'userdata.hdf5'):
-    f = h5py.File(fname, 'a')
-    dset = f.create_dataset("model", (100,), dtype='i')
-    dset.attrs['baeke_work'] = self.baeke_work
-    dset.attrs['baeke_leisure'] = self.baeke_leisure
-    dset.attrs['baeke_sport'] = self.baeke_sport
-
-  def import_hdf5(self, fname = 'userdata.hdf5'):
-    f = h5py.File(fname, 'r')
+    f = h5py.File(fname, 'w')
     dset = f.create_dataset("model", (100,), dtype='i')
     dset.attrs['baeke_work'] = self.baeke_work
     dset.attrs['baeke_leisure'] = self.baeke_leisure
