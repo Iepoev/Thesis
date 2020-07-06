9e75ae9b9959f6a3a53dd5f6f7058075b2aeb885
Iepoev <jeroen.declerck@ugent.be>
2020-07-06 19:14:05 +0200

    semi werkende filter

--- implementatie/data/data_reader.py
+++ implementatie/data/data_reader.py
@@ -4,6 +4,8 @@ import math
 import operator
 import matplotlib.pyplot as plt
 
+ACCEPTABLE_RANGE = 0.1
+
 class Datareader():
 
   def __init__(self):
@@ -27,7 +29,6 @@ class Datareader():
     self.max_hr = 0
     self.current_hr = 0
     self.current_hrv = 0
-    self.prev_ms = 0
     self.sdnn = 0
     self.sdsd = 0
     self.nn50 = 0
@@ -43,10 +44,11 @@ class Datareader():
   def save_to_npy(self, data, fname="data.npy"):
     np.save(fname, data)
 
-
-  def draw(self, data, input_fname="data.npy", output_fname="graph.png"):
+  def draw(self, input_fname="data.npy", output_fname="graph.png"):
     data = np.load(input_fname)
+    self.draw(data, output_fname)
 
+  def draw(self, data, output_fname="graph.png"):
     plt.plot(data[:,0]/60000,data[:,1], linewidth=0.5) # ms
     # plt.plot(data[:,0]/60000,data[:,2], linewidth=0.5) # HR
     # plt.plot(data[:,0]/60000,data[:,3], linewidth=0.5) # HRV (weighted moving average)
@@ -55,6 +57,7 @@ class Datareader():
     # plt.plot(data[:,0]/60000,data[:,6], linewidth=0.5) # self.NN50
     # plt.plot(data[:,0]/60000,data[:,7], linewidth=0.5) # self.rMSSD
     plt.ylabel('dinken')
+    plt.ylim((0,1000))
     plt.savefig(output_fname, bbox_inches='tight', dpi=300)
 
   def handle_ms(self, ms):
@@ -64,6 +67,9 @@ class Datareader():
 
       deltas_10s = np.array(list(map(lambda x, y: abs(x-y), self.ms_10s_window[1:], self.ms_10s_window[:-1])))
       self.current_hrv = np.average(deltas_10s, weights=range(len(self.ms_10s_window)-1))
+
+      if sum(self.ms_120s_window) < 120000:
+        self.current_hr = 60000 / np.average(self.ms_10s_window, weights=range(len(self.ms_10s_window)))
 
     # self.ms_60s_window.append(ms)
     # while sum(self.ms_60s_window) > 60000: #
@@ -99,35 +105,150 @@ class Datareader():
     return [self.total_ms, ms, self.current_hr, self.current_hrv, self.sdnn, self.sdsd, self.nn50, self.rmssd]
 
 
-  def read_raw(self):
+  def read_raw(self, fname='raw/2020-06-30 18-00-07 - felix.txt'):
     A = np.empty((0,8), dtype=float)
 
-    with open('raw/squat_warmup.txt', 'r') as file:
-      self.prev_ms = int(file.readline())
+    with open(fname, 'r') as file:
+
+      queue = []
+      ms_7beat_window = []
+      is_missing = False
 
       for l in file:
-        ms = int(l)
-        self.total_ms += ms
-        ms_diff = abs(self.prev_ms - ms)
+        new_ms = int(l)
+        ms_7beat_window.append(new_ms)
 
-        # if ms_diff <= 100 or ms_diff <= self.current_hrv * 2:
-        if True:
-          print(f"{self.prev_ms - ms}, {self.current_hrv}")
+
+        # 7 beat window:
+        # - always append new interval reading (index 6)
+        # - check if the interval at index 3 is valid
+        # - if something is wrong, try to correct
+        # - handle the earliest interval in the queue (index 0)
+
+        # things that can go wrong:
+        # - sudden HRV change; the following intervals are in the same range
+        #   -> do nothing
+        # - previous or following interval averages to correct range
+        #   -> one incorrectly measured beat
+        #   -> average out both beats
+        # - interval is double of the expected range
+        #   -> missed a beat
+        #   -> halve the interval
+
+        while len(ms_7beat_window) >= 7:
+          print(f"{ms_7beat_window}, hrv:{self.current_hrv}")
+
+          checking_ms = ms_7beat_window[3]
+          ms_diff = abs(ms_7beat_window[2]-ms_7beat_window[3])
+
+          if ms_diff >= 30 or (self.current_hrv >=10 and ms_diff >= self.current_hrv * 2):
+            future_avg = np.average(ms_7beat_window[4:])
+            incorrect_beat = (ms_7beat_window[2] + checking_ms) / 2
+            incorrect_with_missed_beat = (ms_7beat_window[2] + checking_ms) / 3
+            
+            if future_avg*(1-ACCEPTABLE_RANGE) <= checking_ms <= future_avg*(1+ACCEPTABLE_RANGE):
+              print(f"missed ms: {checking_ms}, diff: {ms_diff}, queue avg: {np.average(ms_7beat_window)}, SUDDEN CHANGE: {future_avg}")
+            
+            elif future_avg*(1-ACCEPTABLE_RANGE) <= incorrect_beat <= future_avg*(1+ACCEPTABLE_RANGE):
+              print(f"missed ms: {checking_ms}, diff: {ms_diff}, queue avg: {np.average(ms_7beat_window)}, INCORRECT BEAT: {incorrect_beat}")
+              ms_7beat_window[2] = incorrect_beat
+              ms_7beat_window[3] = incorrect_beat
+            
+            elif future_avg*(1-ACCEPTABLE_RANGE) <= incorrect_with_missed_beat <= future_avg*(1+ACCEPTABLE_RANGE):
+              print(f"missed ms: {checking_ms}, diff: {ms_diff}, queue avg: {np.average(ms_7beat_window)}, INCORRECT WITH MISSED BEAT: {incorrect_with_missed_beat}")
+              ms_7beat_window[2] = incorrect_with_missed_beat
+              ms_7beat_window[3] = incorrect_with_missed_beat
+              ms_7beat_window.insert(3, incorrect_with_missed_beat)
+
+            elif future_avg*(1-ACCEPTABLE_RANGE) <= checking_ms/2 <= future_avg*(1+ACCEPTABLE_RANGE):
+              print(f"missed ms: {checking_ms}, diff: {ms_diff}, queue avg: {np.average(ms_7beat_window)}, MISSED BEAT: {checking_ms/2}")
+              ms_7beat_window[3] = checking_ms/2
+              ms_7beat_window.insert(3, checking_ms/2)
+
+            elif future_avg*(1-ACCEPTABLE_RANGE) <= checking_ms/3 <= future_avg*(1+ACCEPTABLE_RANGE):
+              print(f"missed ms: {checking_ms}, diff: {ms_diff}, queue avg: {np.average(ms_7beat_window)}, DOUBLE MISSED BEAT: {checking_ms/3}")
+              ms_7beat_window[3] = checking_ms/3
+              ms_7beat_window.insert(3, checking_ms/3)
+              ms_7beat_window.insert(3, checking_ms/3)
+
+            else:
+              print(f"missed ms: {checking_ms}, diff: {ms_diff}, queue avg: {np.average(ms_7beat_window)}, incorrect_beat: {incorrect_beat}, triple_incorrect_beat: {incorrect_with_missed_beat}, missed_beat: {ms_7beat_window[3]/2}, future_avg: {future_avg} ?????????")
+              #TODO: fix total_ms
+              #self.total_ms += ms_7beat_window.pop(3)
+              pass
+
+          ms = ms_7beat_window.pop(0)
+
+          self.total_ms += ms
           newrow = self.handle_ms(ms)
-
           A = np.append(A, [newrow], axis=0)
-        else:
-          print(f">>>>>>>> {self.prev_ms - ms}")
 
-        self.prev_ms = ms
+            # if is_missing:
+            #   is_missing = False
+
+          # elif self.current_hr != 0 and (queue == [] or is_missing):
+          #   queue.append(ms)
+          #   queue_avg = (np.sum(queue)+ms)/(len(queue)+1)
+          #   reference_ms = 60000 / self.current_hr
+          #   if reference_ms*0.9 <= queue_avg <= reference_ms*1.1:
+          #     print(f"missed diff: {self.prev_ms - ms}, queue avg: {queue_avg}, reference ms: {reference_ms}, CORRECTING")
+          #     for missed_ms in queue:
+          #       newrow = self.handle_ms(queue_avg)
+          #       A = np.append(A, [newrow], axis=0)
+          #   else:
+          #     print(f"missed diff: {self.prev_ms - ms}, queue avg: {queue_avg}, reference ms: {reference_ms}, CONTINUEING")
+          #     print(queue)
+          #     is_missing = True
+
+          # else:
+          #   print(f"skipped diff: {self.prev_ms - ms}")
+
+
 
     user = [self.resting_hr, self.max_hr]
 
     print(user)
     #print(A)
     np.save('data.npy', A)
+    return A
 
+
+  def find_correction(self, ms_7beat_window):
+    target = (ms_7beat_window[0] + ms_7beat_window[6]) / 2 
+
+    checking_ms = ms_7beat_window[3]
+
+    missed_beat = 0
+    doube_missed_beat
+    incorrect_beat = (ms_7beat_window[2] + checking_ms) / 2
+    incorrect_with_missed_beat = (ms_7beat_window[2] + checking_ms) / 3
+
+
+    if future_avg*(1-ACCEPTABLE_RANGE) <= incorrect_beat <= future_avg*(1+ACCEPTABLE_RANGE):
+      print(f"missed ms: {checking_ms}, diff: {ms_diff}, queue avg: {np.average(ms_7beat_window)}, INCORRECT BEAT: {incorrect_beat}")
+      ms_7beat_window[2] = incorrect_beat
+      ms_7beat_window[3] = incorrect_beat
+    
+    elif future_avg*(1-ACCEPTABLE_RANGE) <= incorrect_with_missed_beat <= future_avg*(1+ACCEPTABLE_RANGE):
+      print(f"missed ms: {checking_ms}, diff: {ms_diff}, queue avg: {np.average(ms_7beat_window)}, INCORRECT WITH MISSED BEAT: {incorrect_with_missed_beat}")
+      ms_7beat_window[3] = incorrect_with_missed_beat
+      ms_7beat_window[4] = incorrect_with_missed_beat
+      ms_7beat_window.insert(4, incorrect_with_missed_beat)
+
+    elif future_avg*(1-ACCEPTABLE_RANGE) <= checking_ms/2 <= future_avg*(1+ACCEPTABLE_RANGE):
+      print(f"missed ms: {checking_ms}, diff: {ms_diff}, queue avg: {np.average(ms_7beat_window)}, MISSED BEAT: {checking_ms/2}")
+      ms_7beat_window[3] = checking_ms/2
+      ms_7beat_window.insert(4, checking_ms/2)
+
+    elif future_avg*(1-ACCEPTABLE_RANGE) <= checking_ms/3 <= future_avg*(1+ACCEPTABLE_RANGE):
+      print(f"missed ms: {checking_ms}, diff: {ms_diff}, queue avg: {np.average(ms_7beat_window)}, DOUBLE MISSED BEAT: {checking_ms/3}")
+      ms_7beat_window[3] = checking_ms/3
+      ms_7beat_window.insert(4, checking_ms/3)
+      ms_7beat_window.insert(4, checking_ms/3)
 
 if __name__ == "__main__":
   reader = Datareader()
-  reader.read_raw()
+  # data = reader.read_raw("/raw/2020-07-01 11-06-32 - arnhoudt")
+  data = reader.read_raw()
+  reader.draw(data, output_fname="graph.png")
+
