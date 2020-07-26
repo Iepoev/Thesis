2ec4e86626e2aebcca30f6089d77dcc192a99167
Iepoev <jeroen.declerck@ugent.be>
2020-07-26 21:53:21 +0200

    disjunct en unieke gebalanceerde batches

--- implementatie/data/datagenerator.py
+++ implementatie/data/datagenerator.py
@@ -19,6 +19,9 @@ class DataGenerator(tensorflow.keras.utils.Sequence):
     self.n_classes = n_classes
     self.data = self.read_data()
 
+    self.min_class_len = 0
+    self.preclassified_data = []
+
     # 2 sequence lenghts of spare space: one seq_len for the actual sequence and one seql_len for when the offset is equal to seq_len
     self.indexes = [i*self.seq_len for i in range(math.floor(len(self.data) / self.seq_len)-2)]
     self.max_offset = len(self.data) % self.seq_len
@@ -46,69 +49,16 @@ class DataGenerator(tensorflow.keras.utils.Sequence):
 
   def __len__(self):
     'Denotes the number of batches per epoch'
-    return int(np.floor(len(self.indexes) / self.batch_size))
+
+    return math.floor((self.min_class_len*self.n_classes) / self.batch_size)
 
   def __getitem__(self, index):
     'Generate one batch of data'
-    # Xx = np.empty((0, self.seq_len, self.n_features),dtype=float)
 
-    X = [np.empty((0, self.seq_len, self.n_features+1),dtype=float)] * self.n_classes
-    # Xy = np.empty((self.n_classes, 0, self.seq_len, self.n_features),dtype=float)
-
-    # idx = self.indexes[0]
-    # seq_start = idx+self.current_offset
-    # seq_end = idx+self.current_offset+self.seq_len
-    # clss = int(self.data[seq_end,-1])
-
-
-    # insert = self.data[seq_start:seq_end,:-1]
-    # # Xx = np.append(Xx, [insert], axis=0) # werkt wel
-    # classified_sequences[0] = np.append(classified_sequences[0], [insert], axis=0) # werkt wel
-    # Xy[0] = np.append(Xy[0], [insert], axis=0) # werkt niet
-    # # print(Xx.shape)
-    # print(classified_sequences[0].shape)
-    # print(insert.shape)
-
-
-    # X[clss] = 
-    # print(self.data[seq_start:seq_end,:-1])
-    # print(X[clss])
-
-    #batch_indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
-    #print(batch_indexes)
-
-    if self.squash_class:
-      for idx in self.indexes:
-        seq_start = idx+self.current_offset
-        seq_end = idx+self.current_offset+self.seq_len
-        clss = int(self.data[seq_end,-1])
-
-        if np.all(self.data[seq_end-10:seq_end,-1] == clss):
-          X[clss] = np.append(X[clss], [self.data[seq_start:seq_end]], axis=0)
-
-    # else:
-    #   y = np.empty((self.batch_size, self.seq_len, self.n_classes), dtype=int)
-
-    #   for i, data_idx in enumerate(batch_indexes):
-
-    #     seq_start = data_idx+self.current_offset
-    #     seq_end = data_idx+self.current_offset+self.seq_len
-
-    #     X[i,] = self.data[seq_start:seq_end,:-1]
-    #     y[i,] = keras.utils.to_categorical(self.data[seq_start:seq_end,-1], num_classes=self.n_classes)
-
-    min_class_len = len(X[0])
-
-    for li in X:
-      if (min_class_len > len(li)): min_class_len = len(li)
-
-    resX = np.empty((self.n_classes*min_class_len, self.seq_len, self.n_features))
-    resy = np.empty((self.n_classes*min_class_len, self.seq_len, self.n_classes))
-
-    for i, li in enumerate(X):
-      resX[i*min_class_len:(i+1)*min_class_len,] = li[:min_class_len,:,:-1]
-      resy[i*min_class_len:(i+1)*min_class_len,] = keras.utils.to_categorical(li[:min_class_len,:,-1], num_classes=self.n_classes)
-
+    resX = self.preclassified_data[index*self.batch_size:(index+1)*self.batch_size,:, :-1]
+    resy = keras.utils.to_categorical(
+      self.preclassified_data[index*self.batch_size:(index+1)*self.batch_size,:,-1], 
+      num_classes=self.n_classes)
 
     return resX, resy
 
@@ -132,3 +82,24 @@ class DataGenerator(tensorflow.keras.utils.Sequence):
   def on_epoch_end(self):
     self.current_offset = random.randrange(self.max_offset)
     np.random.shuffle(self.indexes)
+
+    X = [np.empty((0, self.seq_len, self.n_features+1),dtype=float)] * self.n_classes
+
+    for idx in self.indexes:
+      seq_start = idx+self.current_offset
+      seq_end = idx+self.current_offset+self.seq_len
+      clss = int(self.data[seq_end,-1])
+
+      if np.all(self.data[seq_end-10:seq_end,-1] == clss):
+        X[clss] = np.append(X[clss], [self.data[seq_start:seq_end]], axis=0)
+
+    self.min_class_len = len(X[0])
+
+    for li in X:
+      if (self.min_class_len > len(li)): self.min_class_len = len(li)
+
+    self.preclassified_data = np.empty((self.n_classes*self.min_class_len, self.seq_len, self.n_features+1))
+    for i, li in enumerate(X):
+      self.preclassified_data[i*self.min_class_len:(i+1)*self.min_class_len,] = li[:self.min_class_len,:,:]
+
+    np.random.shuffle(self.preclassified_data)
--- implementatie/main.py
+++ implementatie/main.py
@@ -32,7 +32,6 @@ def seq_to_seq_classification():
   print(inp[31][0])
   print(outp.shape)
 
-
   # LSTM for sequence classification 
   model = Sequential()
   model.add(LSTM(32,return_sequences=True, input_shape=(training_generator.seq_len,16)))
@@ -84,46 +83,48 @@ def deepheart():
 
   n_features = 11
   n_classes = 3
+  seq_len = 64
 
   # Generators
-  training_generator = DataGenerator(squash_class=True, n_features=n_features, seq_len=64, n_classes=n_classes)
-  test_generator = DataGenerator(train=False, squash_class=True, n_features=n_features, seq_len=64, n_classes=n_classes)
+  training_generator = DataGenerator(squash_class=True, n_features=n_features, seq_len=seq_len, n_classes=n_classes)
+  test_generator = DataGenerator(train=False, squash_class=True, n_features=n_features, seq_len=seq_len, n_classes=n_classes)
 
   (inp, outp) = training_generator.__getitem__(0)
   print(inp.shape)
   print(outp.shape)
 
-  # i = Input(shape=(training_generator.seq_len, n_features))
-  # x = TCN(return_sequences=True, kernel_size=12, nb_filters=64,use_batch_norm=True)(i)  # The TCN layers are here.
+  i = Input(shape=(training_generator.seq_len, n_features))
+  x = TCN(return_sequences=True, kernel_size=12, nb_filters=seq_len,use_batch_norm=True)(i)  # The TCN layers are here.
+  x = Dropout(0.2)(x)
+  # x = MaxPooling1D(pool_size=2)(x)
+  # x = TCN(return_sequences=True, kernel_size=5, nb_filters=seq_len,use_batch_norm=True)(x)  # The TCN layers are here.
   # x = Dropout(0.2)(x)
   # x = MaxPooling1D(pool_size=2)(x)
-  # x = TCN(return_sequences=True, kernel_size=5, nb_filters=64,use_batch_norm=True)(x)  # The TCN layers are here.
+  # x = TCN(return_sequences=True, kernel_size=5, nb_filters=seq_len,use_batch_norm=True)(x)  # The TCN layers are here.
   # x = Dropout(0.2)(x)
   # x = MaxPooling1D(pool_size=2)(x)
-  # x = TCN(return_sequences=True, kernel_size=5, nb_filters=64,use_batch_norm=True)(x)  # The TCN layers are here.
-  # x = Dropout(0.2)(x)
-  # x = MaxPooling1D(pool_size=2)(x)
-  # # x = Bidirectional(LSTM(64, return_sequences=True))(x)
-  # # x = Bidirectional(LSTM(64, return_sequences=True))(x)
-  # # x = Bidirectional(LSTM(64, return_sequences=True))(x)
-  # x = Bidirectional(LSTM(64, return_sequences=False))(x)
-  # x = Dropout(0.2)(x)
-  # # x = Conv1D(filters=training_generator.n_classes, kernel_size=64, activation='tanh')(x)
-  # x = Dense(training_generator.n_classes, activation='softmax')(x)
+  # x = Bidirectional(LSTM(seq_len, return_sequences=True))(x)
+  # x = Bidirectional(LSTM(seq_len, return_sequences=True))(x)
+  # x = Bidirectional(LSTM(seq_len, return_sequences=True))(x)
+  # x = Bidirectional(LSTM(seq_len, return_sequences=False))(x)
+  x = Dropout(0.2)(x)
+  # x = Conv1D(filters=training_generator.n_classes, kernel_size=seq_len, activation='tanh')(x)
+  # x = Dense(training_generator.n_classes, activation='softmax')(i)
+  x = Dense(training_generator.n_classes, activation='softmax')(x)
 
-  # model = Model(inputs=[i], outputs=[x])
+  model = Model(inputs=[i], outputs=[x])
 
-  # model.summary()
+  model.summary()
 
-  # # try using different optimizers and different optimizer configs
-  # model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
+  # try using different optimizers and different optimizer configs
+  model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
 
-  # model.fit(training_generator, epochs=20, callbacks=[tensorboard_callback])
+  model.fit(training_generator, epochs=20, callbacks=[tensorboard_callback])
 
   # print(model.predict(test_generator.__getitem__(0)))
 
-  # scores = model.evaluate(test_generator, verbose=1)
-  # print(scores)
+  scores = model.evaluate(test_generator, verbose=1)
+  print(scores)
 
 
 def main():
